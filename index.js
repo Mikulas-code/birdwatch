382e585e690c8cafd26a6ae7ffa9d4d40929f726
Mikulas <bilm18@vse.cz>
2026-05-27 17:40:53 +0200

    feat: pridan formular na pridavani novych ptaku do seznamu

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = drizzle({
 const app = new Hono();
 
 
-
+//endpoints
 app.get("/", async (c) => {
   const birds = await db.select().from(birdsTable).all();
   const index = await ejs.renderFile("src/views/index.ejs", {
@@ -26,3 +26,33 @@ app.get("/", async (c) => {
 serve(app, (info) => {
   console.log(`Server listening at http://localhost:${info.port}`);
 });
+
+app.post('/add-bird', async (c) => {
+  const body = await c.req.formData();
+  const name = body.get('name');
+  const latinName = body.get('latinName');
+  const order = body.get('order');
+  const family = body.get('family');
+  const date = body.get('date');
+  const gender = body.get('gender') === 'true';
+  const notes = body.get('notes');
+  const count = body.get('count');
+  const seen = body.get('seen') === 'true';
+  const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
+  const userId = 1;
+  await db.insert(birdsTable).values({
+    name,
+    latinName,
+    order,
+    family,
+    date,
+    gender,
+    notes,
+    count,
+    seen,
+    imageURL,
+    userId,
+  })
+
+  return c.redirect('/')
+})
