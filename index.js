33eef35cdce9cdc2f8df5af916994e39e33669ce
Mikulas Bilek <bilm18@vse.cz>
2026-05-30 15:15:48 +0200

    feat: pridan edit na detailu ptaka

--- index.js
+++ index.js
@@ -79,3 +79,52 @@ app.post('/toggle-bird/:id', async (c) => {
 
   return c.redirect('/');
 })
+
+// open edit
+app.get('/open-edit/:id', async (c)=>{
+  const id = Number(c.req.param('id'));
+  const bird = await db.select().from(birdsTable).where(eq(birdsTable.id, id)).get();
+  const edit = await ejs.renderFile("src/views/edit.ejs", {
+    title: "Edit " + bird.name,
+    bird
+  });
+
+  return c.html(edit);
+})
+
+// save changes
+app.post('/save-changes/:id', async (c) => {
+  const body = await c.req.formData();
+  const id = Number(c.req.param('id'));
+  const bird = await db.select().from(birdsTable).where(eq(birdsTable.id, id)).get();
+
+  const name = body.get('name');
+  const latinName = body.get('latinName');
+  const order = body.get('order');
+  const family = body.get('family');
+  const date = body.get('date');
+  const gender = body.get('gender') === 'true';
+  const notes = body.get('notes');
+  const count = body.get('count');
+  const seen = body.get('seen') === 'true';
+  const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
+  const userId = 1;
+
+
+  await db.update(birdsTable).set({
+    name: name,
+    latinName: latinName,
+    order: order,
+    family: family,
+    date: date,
+    gender: gender,
+    notes: notes,
+    count: count,
+    seen: seen,
+    imageURL: imageURL,
+    userId: userId
+  }).where(eq(birdsTable.id, id));
+
+
+  return c.redirect('/')
+})
