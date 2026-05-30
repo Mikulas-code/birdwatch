cc8659e611868da79060e4e55cfc3147f10dad85
Mikulas Bilek <bilm18@vse.cz>
2026-05-30 16:02:50 +0200

    feat: pridany styly s tailwindem

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import ejs from "ejs";
-import { drizzle } from "drizzle-orm/libsql"
+import { drizzle } from "drizzle-orm/libsql";
 import { birdsTable } from "./src/schema.js";
-import { eq } from "drizzle-orm"
+import { eq } from "drizzle-orm";
 
 const db = drizzle({
   connection: "file:db.sqlite",
   logger: true,
-})
+});
 
 const app = new Hono();
-
 
 //endpoints
 // Homepage
 app.get("/", async (c) => {
   const birds = await db.select().from(birdsTable).all();
-  const index = await ejs.renderFile("src/views/index.ejs", {
-    title: "Birds",
-    birds
-  });
+  const index = await ejs.renderFile(
+    "src/views/index.ejs",
+    {
+      title: "Birds",
+      birds,
+    },
+    { views: ["src/views"] },
+  );
 
   return c.html(index);
 });
@@ -29,20 +32,20 @@ serve(app, (info) => {
   console.log(`Server listening at http://localhost:${info.port}`);
 });
 
-
 // pridavani ptaku
-app.post('/add-bird', async (c) => {
+app.post("/add-bird", async (c) => {
   const body = await c.req.formData();
-  const name = body.get('name');
-  const latinName = body.get('latinName');
-  const order = body.get('order');
-  const family = body.get('family');
-  const date = body.get('date');
-  const gender = body.get('gender') === 'true';
-  const notes = body.get('notes');
-  const count = body.get('count');
-  const seen = body.get('seen') === 'true';
-  const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
+  const name = body.get("name");
+  const latinName = body.get("latinName");
+  const order = body.get("order");
+  const family = body.get("family");
+  const date = body.get("date");
+  const gender = body.get("gender") === "true";
+  const notes = body.get("notes");
+  const count = body.get("count");
+  const seen = body.get("seen") === "true";
+  const imageURL =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
   const userId = 1;
   await db.insert(birdsTable).values({
     name,
@@ -56,75 +59,97 @@ app.post('/add-bird', async (c) => {
     seen,
     imageURL,
     userId,
-  })
+  });
 
-  return c.redirect('/')
-})
+  return c.redirect("/");
+});
 
 // mazani ptaku
-app.post('/remove-bird/:id', async (c) => {
-  const id = Number(c.req.param('id'));
+app.post("/remove-bird/:id", async (c) => {
+  const id = Number(c.req.param("id"));
 
   await db.delete(birdsTable).where(eq(birdsTable.id, id));
 
-  return c.redirect('/');
-})
+  return c.redirect("/");
+});
 // zmena stavu
-app.post('/toggle-bird/:id', async (c) => {
-  const id = Number(c.req.param('id'));
+app.post("/toggle-bird/:id", async (c) => {
+  const id = Number(c.req.param("id"));
 
-  const bird = await db.select().from(birdsTable).where(eq(birdsTable.id, id)).get();
+  const bird = await db
+    .select()
+    .from(birdsTable)
+    .where(eq(birdsTable.id, id))
+    .get();
 
-  await db.update(birdsTable).set({ seen: !bird.seen }).where(eq(birdsTable.id, id));
+  await db
+    .update(birdsTable)
+    .set({ seen: !bird.seen })
+    .where(eq(birdsTable.id, id));
 
-  return c.redirect('/');
-})
+  return c.redirect("/");
+});
 
 // open edit
-app.get('/open-edit/:id', async (c)=>{
-  const id = Number(c.req.param('id'));
-  const bird = await db.select().from(birdsTable).where(eq(birdsTable.id, id)).get();
-  const edit = await ejs.renderFile("src/views/edit.ejs", {
-    title: "Edit " + bird.name,
-    bird
-  });
+app.get("/open-edit/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+  const bird = await db
+    .select()
+    .from(birdsTable)
+    .where(eq(birdsTable.id, id))
+    .get();
+
+  const edit = await ejs.renderFile(
+    "src/views/edit.ejs",
+    {
+      title: "Edit " + bird.name,
+    bird,
+    },
+    { views: ["src/views"] },
+  );
 
   return c.html(edit);
-})
+});
 
 // save changes
-app.post('/save-changes/:id', async (c) => {
+app.post("/save-changes/:id", async (c) => {
   const body = await c.req.formData();
-  const id = Number(c.req.param('id'));
-  const bird = await db.select().from(birdsTable).where(eq(birdsTable.id, id)).get();
+  const id = Number(c.req.param("id"));
+  const bird = await db
+    .select()
+    .from(birdsTable)
+    .where(eq(birdsTable.id, id))
+    .get();
 
-  const name = body.get('name');
-  const latinName = body.get('latinName');
-  const order = body.get('order');
-  const family = body.get('family');
-  const date = body.get('date');
-  const gender = body.get('gender') === 'true';
-  const notes = body.get('notes');
-  const count = body.get('count');
-  const seen = body.get('seen') === 'true';
-  const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
+  const name = body.get("name");
+  const latinName = body.get("latinName");
+  const order = body.get("order");
+  const family = body.get("family");
+  const date = body.get("date");
+  const gender = body.get("gender") === "true";
+  const notes = body.get("notes");
+  const count = body.get("count");
+  const seen = body.get("seen") === "true";
+  const imageURL =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg";
   const userId = 1;
 
+  await db
+    .update(birdsTable)
+    .set({
+      name: name,
+      latinName: latinName,
+      order: order,
+      family: family,
+      date: date,
+      gender: gender,
+      notes: notes,
+      count: count,
+      seen: seen,
+      imageURL: imageURL,
+      userId: userId,
+    })
+    .where(eq(birdsTable.id, id));
 
-  await db.update(birdsTable).set({
-    name: name,
-    latinName: latinName,
-    order: order,
-    family: family,
-    date: date,
-    gender: gender,
-    notes: notes,
-    count: count,
-    seen: seen,
-    imageURL: imageURL,
-    userId: userId
-  }).where(eq(birdsTable.id, id));
-
-
-  return c.redirect('/')
-})
+  return c.redirect("/");
+});
